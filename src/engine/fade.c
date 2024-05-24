50078bb3fae0c55e5fc7e8236172df8f70808d38
Rosie Sapphire <jayjaygames40@gmail.com>
2024-05-23 22:57:19 -0700

    Upgraded the fading effect to use a 64x64 IA4 texture instead of a 32x32 RGBA16 texture

--- src/engine/fade.c
+++ src/engine/fade.c
@@ -4,42 +4,27 @@
 #include "engine/util.h"
 #include "engine/fade.h"
 
-#define FADE_SURF_WIDTH 32
-#define FADE_SURF_HEIGHT 32
+#define FADE_SURF_WIDTH 64
+#define FADE_SURF_HEIGHT 64
 #define FADE_SURF_PIXEL_CNT (DISPLAY_WIDTH * DISPLAY_HEIGHT)
 
-#define FADE_RADIUS_MAX 512
+#define FADE_RADIUS_MAX 420
 #define FADE_SPEED 16
 
-static uint16_t surf_pixels[FADE_SURF_PIXEL_CNT];
-static surface_t surf;
+static sprite_t *spr_circle;
 
 static int fade_state = FADE_STATE_DISABLED;
 static int fade_radius = FADE_RADIUS_MAX;
 
 void fade_load_spr(void)
 {
-	for (int y = 0; y < FADE_SURF_HEIGHT; y++)
-	{
-		for (int x = 0; x < FADE_SURF_WIDTH; x++)
-		{
-			const int dir_x = x - (FADE_SURF_WIDTH >> 1);
-			const int dir_y = y - (FADE_SURF_HEIGHT >> 1);
-			const int dist_sq = dir_x * dir_x + dir_y * dir_y;
-			const int ind = y * FADE_SURF_WIDTH + x;
-
-			surf_pixels[ind] = (dist_sq < 16 * 16) * 0xFFFF;
-		}
-	}
-	surf = surface_make_linear(surf_pixels, FMT_RGBA16,
-				   FADE_SURF_WIDTH, FADE_SURF_HEIGHT);
-	data_cache_writeback_invalidate_all();
+	spr_circle = sprite_load("rom:/fade_circle.ia4.sprite");
 }
 
 void fade_state_setup(const int fade_state_new)
 {
 	const int fade_radius_new[3] = {
-		0, 0, 512,
+		0, 0, FADE_RADIUS_MAX,
 	};
 
 	fade_state = fade_state_new;
@@ -55,11 +40,13 @@ int fade_update(const int should_switch)
 
 	fade_state_setup(fade_state ^ should_switch);
 	fade_radius = fade_radius_new[fade_state];
-	if (fade_radius < 0)
+	if ((fade_radius = MAX(fade_radius, FADE_RADIUS_MAX)) < 0)
 	{
 		fade_radius = 0;
 		return (1);
 	}
+
+	debugf("%d\n", fade_radius);
 
 	return (0);
 }
@@ -84,7 +71,8 @@ void fade_render(void)
 	rdpq_mode_blender(RDPQ_BLENDER((MEMORY_RGB, IN_ALPHA,
 					BLEND_RGB, ZERO)));
 	rdpq_mode_filter(FILTER_BILINEAR);
-	rdpq_tex_upload(TILE0, &surf, NULL);
+	rdpq_sprite_upload(TILE0, spr_circle, NULL);
+	rdpq_mode_dithering(DITHER_BAYER_NONE);
 	rdpq_texture_rectangle_scaled(TILE0,
 				      rect[0], rect[1],  rect[2], rect[3],
 				      0, 0, FADE_SURF_WIDTH, FADE_SURF_HEIGHT);

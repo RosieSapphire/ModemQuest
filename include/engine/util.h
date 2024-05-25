f7037d19c254bb15295a304258a0797c49e282c9
Rosie Sapphire <jayjaygames40@gmail.com>
2024-05-25 13:52:39 -0700

    Added a custom rdpq function that just prints a rect with an outline

--- include/engine/util.h
+++ include/engine/util.h
@@ -1,11 +1,18 @@
 #ifndef _ENGINE_UTIL_H_
 #define _ENGINE_UTIL_H_
 
+#include <stdint.h>
+
 #define UNUSED __attribute__((unused))
+
 #define MIN(X, MINV) (X < MINV ? MINV : X)
 #define MAX(X, MAXV) (X > MAXV ? MAXV : X)
 #define CLAMP(X, MINV, MAXV) (MAX(MIN(X, MINV), MAXV))
 
 float lerpf(const float a, const float b, const float t);
+void rdpq_fill_rect_border(const int x0, const int y0,
+			   const int x1, const int y1,
+			   const uint8_t r, const uint8_t g, const uint8_t b,
+			   const uint8_t thick);
 
 #endif /* _ENGINE_UTIL_H_ */
--- src/engine/util.c
+++ src/engine/util.c
@@ -1,6 +1,25 @@
+#include <libdragon.h>
+
 #include "engine/util.h"
 
 float lerpf(const float a, const float b, const float t)
 {
 	return (a + (b - a) * t);
+}
+
+void rdpq_fill_rect_border(const int x0, const int y0,
+			   const int x1, const int y1,
+			   const uint8_t r, const uint8_t g, const uint8_t b,
+			   const uint8_t thick)
+{
+	const color_t lighter = RGBA16(r, g, b, 0x1);
+	const color_t darker = RGBA16(r >> 1, g >> 1, b >> 1, 0x1);
+
+	rdpq_set_mode_fill(lighter);
+	rdpq_fill_rectangle(x0 + thick, y0 + thick, x1 - thick, y1 - thick);
+	rdpq_set_fill_color(darker);
+	rdpq_fill_rectangle(x0, y0, x0 + thick, y1); /* left */
+	rdpq_fill_rectangle(x1 - thick, y0, x1, y1); /* right */
+	rdpq_fill_rectangle(x0 + thick, y0, x1 - thick, y0 + thick); /* up */
+	rdpq_fill_rectangle(x0 + thick, y1 - thick, x1 - thick, y1); /* down */
 }
